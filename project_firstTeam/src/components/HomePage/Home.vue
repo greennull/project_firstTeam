548c633be3e36b0a1262ae1e6127572a87d402fe
greennull <285399141@qq.com>
2019-11-11 09:55:02 +0800

    添加路由信息，配置基调

---
+++ project_firstTeam/src/components/HomePage/Home.vue
@@ -0,0 +1,29 @@
+<template>
+
+       
+    <div id=""> {{msg}}</div>
+
+</template>
+
+
+<script>
+
+    export default {
+        name: '',
+        data() {
+
+            return {msg: 'vue模板页'}
+
+        },
+        mounted() {
+        }
+
+    }
+
+</script>
+
+
+<style>
+
+
+</style>
